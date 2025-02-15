510118c42a886877fbbcec2d50b960a6ac5b408e
ultfone <144716513+ultfone@users.noreply.github.com>
2025-02-16 00:48:10 +0530

    Removed some meaningless comments

--- main.py
+++ main.py
@@ -20,8 +20,7 @@ def listen_part():
             model=genai.GenerativeModel("gemini-1.5-flash")
             response=model.generate_content(text)
             a = response.text            
-            print(f"Gemini:{a if response else 'No response from Gemini.'}")
-            # return response.text if response else "No response from Gemini."        
+            print(f"Gemini:{a if response else 'No response from Gemini.'}")        
             tts_fr = gTTS(a, lang='fr',slow=False)
             tts_fr.save("GG.mp3")
             subprocess.run(["ffmpeg", "-i", "GG.mp3", "-filter:a", "atempo=1.5", "-y", "faster.mp3"])
@@ -41,22 +40,7 @@ def listen_part():
             print("Maintenance")
         except Exception as e:
             print("error(cooked fr)",e)
-    # model=genai.GenerativeModel("gemini-1.5-flash")
-    # response=model.generate_content(text)
-    # print(f"Gemini:{response.text if response else 'No response from Gemini.'}")
-    # # return response.text if response else "No response from Gemini."        
-    # tts_fr = gTTS(response, lang='fr')
-#     # tts_fr.save("GG.mp3")
-# listen_part()
-# pg.mixer.init()
-# pg.mixer.music.load("GG.mp3")
-# pg.mixer.music.set_volume(1.0)  
-# pg.mixer.music.play()
 
-# while pg.mixer.music.get_busy():
-#     # pygame.time.Clock().tick(10) 
-#     '''uncomment above  if trash pc'''
-#     pass
 
 while True :
     listen_part()
